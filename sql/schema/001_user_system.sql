711f8f0caef12a2a5b82c440975c9c20e2d41563
Hasan <123514871+imhasandl@users.noreply.github.com>
2025-03-19 20:34:57 +0500

    migrations

--- sql/schema/001_user_system.sql
+++ sql/schema/001_user_system.sql
@@ -6,12 +6,26 @@ CREATE TABLE users (
    email TEXT NOT NULL UNIQUE,
    password TEXT NOT NULL,
    username TEXT NOT NULL,
+   subscribers UUID[],
+   subscribed_to UUID[],
    is_premium BOOLEAN NOT NULL DEFAULT FALSE,
    verification_code INT NOT NULL,
    is_verified BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE INDEX idx_users_username ON users(username);
+
+CREATE TABLE device_tokens (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    device_token TEXT NOT NULL,
+    device_type TEXT NOT NULL, -- e.g., 'android', 'ios', 'web'
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(user_id, device_token)
+);
+
+CREATE INDEX idx_device_tokens_user_id ON device_tokens(user_id);
 
 CREATE TABLE refresh_tokens (
     token VARCHAR(255) PRIMARY KEY,
@@ -21,6 +35,8 @@ CREATE TABLE refresh_tokens (
 );
 
 -- +goose Down
-DROP TABLE refresh_tokens;
-DROP INDEX idx_users_username;
 DROP TABLE users;
+DROP INDEX idx_users_username;
+DROP TABLE device_token;
+DROP INDEX idx_device_tokens_user_id;
+DROP TABLE refresh_tokens;
